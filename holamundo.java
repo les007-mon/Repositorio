6b068c064d86f36262ab0ff28338516261831c13
noe555-coder <25022100038@uppenjamo.edu.mx>
2026-07-16 12:51:13 -0600

    Add greeting message to holamundo.java

--- holamundo.java
+++ holamundo.java
@@ -3,6 +3,7 @@ public class HolaMundo {
         System.out.println("¡Hola, leslie como estas me caes mal te odio!");
          System.out.println("¡oooooooooo!");
         System.out.println("¡buenas tardes ADIOS!");
+        System.out.println("ola lesli");
     }
 }
 
