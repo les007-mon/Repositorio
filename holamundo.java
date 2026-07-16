0ecbae1de14457124a951cdc4bc69bfc2cd6edd1
les007-mon <25032100056@uppenjamo.edu.mx>
2026-07-16 12:36:15 -0600

    Update holamundo.java

--- holamundo.java
+++ holamundo.java
@@ -2,6 +2,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("¡Hola, leslie como estas me caes mal te odio!");
          System.out.println("¡oooooooooo!");
+        System.out.println("¡buenas tardes ADIOS!");
     }
 }
 
